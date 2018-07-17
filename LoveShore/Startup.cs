4a8152ea3295222e354b30376ef17a4277cba5ce
jyothish <jyothish@JYOTHISH-PC>
2018-07-17 17:40:39 +0530

    Add project files.

---
+++ LoveShore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LoveShore.Startup))]
+namespace LoveShore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
